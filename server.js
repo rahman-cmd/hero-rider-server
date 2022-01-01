e8bcddc24e0945b63bd003b382d2bba6016a0ecc
MD ABDUR RAHMAN <mmm.rahman77@gmail.com>
2022-01-02 00:58:33 +0600

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json({ limit: 2000000 }));
 app.use(fileUpload());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phero-crud.9f5td.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnelf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
    useNewUrlParser: true,
